5e88eb29761bc20928145542766939dd1efdb98b
Kaan Karaman <kaankaraman35@gmail.com>
2020-08-19 19:36:56 +0300

    Index page with youtube search form added

---
+++ src/Controller/PageController.php
@@ -0,0 +1,12 @@
+<?php
+	namespace App\Controller;
+	use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+	use Symfony\Component\Routing\Annotation\Route;
+	class PageController extends AbstractController {
+		/**
+		 * @Route("/", name="index")
+		 */
+		public function index() {
+			return $this->render('page/index.html.twig');
+		}
+	}
